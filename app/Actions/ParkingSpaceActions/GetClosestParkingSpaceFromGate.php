e9a2ac113e06d71212a179f28f28a404754c45d5
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-17 21:26:12 +0800

    docs: added comments for GetClosestParkingSpaceFromGate.php

--- app/Actions/ParkingSpaceActions/GetClosestParkingSpaceFromGate.php
+++ app/Actions/ParkingSpaceActions/GetClosestParkingSpaceFromGate.php
@@ -7,6 +7,14 @@ use App\Utilities\NumberUtilities;
 
 class GetClosestParkingSpaceFromGate
 {
+    /**
+     * Gets the closest available parking space for the vehicle coming in by getting all the available parking space
+     * and determining the nearest space from the gate the vehicle has entered by
+     *
+     * @param integer $nearestSpaceFromGate ID of nearest parking space from the gate. Can only get this from the `gates` table
+     * @param integer $vehicleTypeId The vehicle type coming in to the parking lot
+     * @return ParkingSpace
+     */
     public function handle(int $nearestSpaceFromGate, int $vehicleTypeId): ParkingSpace
     {
         $utilities = new NumberUtilities();
