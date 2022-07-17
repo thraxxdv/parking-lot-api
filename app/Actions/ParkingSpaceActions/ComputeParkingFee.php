1e724b873fddb18b66bae3c41986df690fe832e5
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-17 21:22:43 +0800

    docs: added comments for ComputeParkingFee.php

--- app/Actions/ParkingSpaceActions/ComputeParkingFee.php
+++ app/Actions/ParkingSpaceActions/ComputeParkingFee.php
@@ -12,7 +12,15 @@ class ComputeParkingFee
     private float $flatRate = 40;
     private float $chunkRate = 5000;
 
-    public function handle(string $parkedOn, string $leftOn, int $vehicleType)
+    /**
+     * Entry (handle) method for computing parking fee
+     *
+     * @param string $parkedOn Timestamp on when the vehicle parks
+     * @param string $leftOn Timestamp on when the vehicle unparks
+     * @param integer $vehicleType The vehicle type for the parking space
+     * @return float Calculated fee
+     */
+    public function handle(string $parkedOn, string $leftOn, int $vehicleType) : float
     {
         $dateUtilities = new DateUtilities();
         $hours = ceil($dateUtilities->getTimeDifference($parkedOn, $leftOn) / 60);
@@ -20,7 +28,15 @@ class ComputeParkingFee
         return $this->calculateFee($hours, $vehicleType);
     }
 
-    private function calculateFee(int $totalHours, int $vehicleTypeId)
+    /**
+     * Calculates fees including parking within 24 hours
+     * and chunk rates
+     *
+     * @param integer $totalHours Total hours parked
+     * @param integer $vehicleTypeId The vehicle type for the parking space
+     * @return float Calculated fee
+     */
+    private function calculateFee(int $totalHours, int $vehicleTypeId) : float
     {
         $flatRate = 0;
         $vehicleRate = 0;
@@ -39,24 +55,53 @@ class ComputeParkingFee
         return $flatRate + $vehicleRate + $chunkRate;
     }
 
-    private function calculateFlatRate(int $hours)
+    /**
+     * Calculates the flat rate
+     *
+     * @param integer $hours Hours applicable to flat rate
+     * @return float
+     */
+    private function calculateFlatRate(int $hours) : float
     {
         return $this->flatRate * $hours;
     }
 
-    private function calculateVehicleRate(int $hours, int $vehicleTypeId)
+    /**
+     * Calculates the parking space specific rate by getting
+     * rates from the database and multiplying it by the
+     * applicable hours
+     *
+     * @param integer $hours Hours applicable for parking space specific rate
+     * @param integer $vehicleTypeId The vehicle type for the parking space
+     * @return float Calculated rate for the vehicle type
+     */
+    private function calculateVehicleRate(int $hours, int $vehicleTypeId) : float
     {
         $vehicleType = VehicleType::find($vehicleTypeId);
         Log::debug($vehicleType);
         return $vehicleType->rate * $hours;
     }
 
-    private function calculateChunkRate(int $multiplier)
+    /**
+     * Takes a multiplier as an argument and
+     * multiplies that by the chunk rate
+     *
+     * @param integer $multiplier
+     * @return float
+     */
+    private function calculateChunkRate(int $multiplier) : float
     {
         return $multiplier * $this->chunkRate;
     }
 
-    private function filter24Hours(int $hours)
+    /**
+     * Returns an array that has computed the applicable hours
+     * for flat rate and parking space specific rates
+     *
+     * @param integer $hours
+     * @return array
+     */
+    private function filter24Hours(int $hours) : array
     {
         $flatHours = 0;
         $vehicleHours = 0;
@@ -73,10 +118,19 @@ class ComputeParkingFee
         ];
     }
 
-    public function filterChunkRate(int $hours)
+    /**
+     * Returns an array that has compute the applicable hours
+     * for flat rate, parking space specific rates as well as
+     * the chunk rate. This method re-uses the `filter24Hours($hours)`
+     * method
+     *
+     * @param integer $hours
+     * @return array
+     */
+    public function filterChunkRate(int $hours) : array
     {
         $chunkableMultiplier = floor($hours / 24);
-        $remainingHours = $hours - ($chunkableMultiplier * 24);
+        $remainingHours = $hours ($chunkableMultiplier * 24);
         $calcs = $this->filter24Hours($remainingHours);
         $calcs['chunk_multiplier'] = $chunkableMultiplier;
         return $calcs;
