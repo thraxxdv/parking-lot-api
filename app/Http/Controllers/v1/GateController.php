19d441aecce7607ef2e21fb68b4009e679952bcb
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-18 21:07:23 +0800

    build: gate creation validation

--- app/Http/Controllers/v1/GateController.php
+++ app/Http/Controllers/v1/GateController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Rules\IsNearestSpaceFromGateTaken;
 use Illuminate\Http\Request;
 
 class GateController extends Controller
 {
-    public function createGate()
+    public function createGate(Request $request)
     {
-        # code...
+        $validated = $request->validate([ 'nearest_space' => [ 'exists:parking_spaces,id', new IsNearestSpaceFromGateTaken ] ]);
     }
 
     public function deleteGate()
