144ff1d14b136274fe38f6b15918f2ed2fec3561
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-21 14:01:21 +0800

    build: added parking space observer

---
+++ app/Observers/ParkingSpaceObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\ParkingSpace;
+
+class ParkingSpaceObserver
+{
+
+    public function created(ParkingSpace $parkingSpace)
+    {
+        //
+    }
+
+    public function updated(ParkingSpace $parkingSpace)
+    {
+        //
+    }
+}
