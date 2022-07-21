e0869bc4adc24f76180ca0e8841b4ffe3797d45b
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-21 21:48:14 +0800

    build: GateUpdated event broadcast configuration

--- app/Events/Gate/GateUpdated.php
+++ app/Events/Gate/GateUpdated.php
@@ -2,35 +2,32 @@
 
 namespace App\Events\Gate;
 
+use App\Services\GateService;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class GateUpdated
+class GateUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('gate-updated');
+    }
+
+    public function broadcastAs()
+    {
+        return 'gate-updated';
+    }
+
+    public function broadcastWith()
+    {
+        $gateService = new GateService();
+        return [
+            'gates' => $gateService->getGates()
+        ];
     }
 }
